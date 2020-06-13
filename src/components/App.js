510c8657eedc63c2de2dcf6f0908df5e797049df
Jose Hernandez-Inunza <jose.hernandez-inzunza@techtonic.com>
2020-06-13 15:55:19 -0600

    scaffolding commit, added dependencies (material-ui, cypress), removing default files from running create-react-app, created basic component structure and files (App -> TodoList > TodoItem), next steps are to create the input component and hook it up with the TodoItem component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoList from './TodoList';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        App!
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+  return (
+    <div>
+      Todo List!
+      <TodoItem />
+    </div>
+  );
+};
+
+export default TodoList;
