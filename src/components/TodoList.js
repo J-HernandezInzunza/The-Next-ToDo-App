786fcca4bf080dff2e7bd4244a6bafb988c23844
Jose Hernandez-Inzunza <42684282+J-HernandezInzunza@users.noreply.github.com>
2020-06-14 00:18:30 -0600

    added main body styling and temp title header, built out ToDoItem component, added basic styling, changed ToDoList into a material-ui List component (#3)
    Added main body styling and temp title header, built out ToDoItem component, added basic styling, changed ToDoList into a material-ui List component (#3)
    
    Co-authored-by: Jose Hernandez Inzunza

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { List } from '@material-ui/core';
 import TodoItem from './TodoItem';
 
 const TodoList = ({ todoList }) => {
   const renderedTodoItems = todoList.map((todoItem) => <TodoItem text={todoItem} />);
 
-  return <div>{renderedTodoItems}</div>;
+  return <List id="todo-list">{renderedTodoItems}</List>;
 };
 
 export default TodoList;
