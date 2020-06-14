3e2777bd87a663e98564d2d3ec6057ba7c147096
Jose Hernandez-Inzunza <42684282+J-HernandezInzunza@users.noreply.github.com>
2020-06-13 20:51:43 -0600

    Feature/create todo input (#2)
    * brought in two additional dependencies (node-sass and material-ui icons), created AddTodoInput component, minimal styling additions for the input
    
    * additional styling changes to the AddToDo Input component, mapped each entry to a new ToDoItem component to be rendered in the ToDoList, next steps are to expand and style the ToDoItem component, right now it is just a div with some text value, once the basic logic is in place I will explore testing the fundamental application
    
    Co-authored-by: Jose Hernandez Inzunza

---
+++ src/components/AddTodoInput.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/AddTodoInput.scss';
+import { Paper, InputBase, Button } from '@material-ui/core';
+import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
+
+class AddTodoInput extends React.Component {
+  state = { inputText: '' };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onFormSubmit(this.state.inputText);
+    this.setState({ inputText: '' });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ inputText: event.target.value });
+  };
+
+  render() {
+    return (
+      <Paper elevation={2} component="form" id="form" onSubmit={this.onFormSubmit}>
+        <InputBase
+          className="input-base"
+          placeholder="Watch random videos on youtube all day..."
+          onChange={this.onInputChange}
+          value={this.state.inputText}
+          required
+        ></InputBase>
+        <Button className="add-button" variant="contained" type="submit">
+          <LibraryAddIcon fontSize="large" />
+        </Button>
+      </Paper>
+    );
+  }
+}
+
+export default AddTodoInput;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoInput from './AddTodoInput';
+import { Container } from '@material-ui/core';
+import '../styles/App.scss';
 
 class App extends React.Component {
+  state = { todoList: [] };
+
+  onTodoSubmit = (todoItem) => {
+    this.setState((state) => {
+      const newTodoList = [...state.todoList, todoItem];
+
+      return { todoList: newTodoList };
+    });
+  };
+
   render() {
     return (
-      <div>
-        App!
-        <TodoList />
-      </div>
+      <Container maxWidth="md" id="container">
+        <AddTodoInput onFormSubmit={this.onTodoSubmit} />
+        <TodoList todoList={this.state.todoList} />
+      </Container>
     );
   }
 }
