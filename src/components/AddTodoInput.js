f35e7f2f9d8f0b8afc41a84220ff33327c17daed
Jose Hernandez-Inzunza <42684282+J-HernandezInzunza@users.noreply.github.com>
2020-06-16 09:36:12 -0600

    Feature/todo input validation
    * added validation to adding todos, ensuring no whitespace entries
    
    * cleanup commit
    
    Co-authored-by: Jose Hernandez Inzunza

--- src/components/AddTodoInput.js
+++ src/components/AddTodoInput.js
@@ -7,20 +7,45 @@ import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
 class AddTodoInput extends React.Component {
-  state = { inputText: '' };
+  state = { inputText: '', error: false, errorText: '' };
 
   onFormSubmit = (event) => {
     event.preventDefault();
+    const { inputText } = this.state;
 
-    this.props.onFormSubmit(this.state.inputText);
-    this.setState({ inputText: '' });
+    if (this.validateInput(inputText)) {
+      this.props.onFormSubmit(inputText);
+      this.setState({ inputText: '' });
+    }
+  };
+
+  validateInput = (input) => {
+    if (input.trim().length === 0) {
+      this.setState({
+        error: true,
+        errorText: 'Todo Text Cannot Be Whitespace or Empty',
+        inputText: '',
+      });
+      setTimeout(() => {
+        this.setState({ error: false });
+      }, 2000);
+      return false;
+    }
+    return true;
   };
 
   onInputChange = (event) => {
     this.setState({ inputText: event.target.value });
+
+    if (event.target.value.length > 100) {
+      this.setState({ error: true, errorText: 'Todo Text Must Be Less Than 100 Characters' });
+    } else {
+      this.setState({ error: false, errorText: '' });
+    }
   };
 
   render() {
+    const { error, errorText, inputText } = this.state;
     return (
       <Paper elevation={2} component="form" id="add-form" onSubmit={this.onFormSubmit}>
         <TextField
@@ -28,12 +53,15 @@ class AddTodoInput extends React.Component {
           className="add-input"
           placeholder="What's Your Next Todo?..."
           onChange={this.onInputChange}
-          value={this.state.inputText}
+          value={inputText}
           required
+          error={error}
+          helperText={error ? `${errorText}` : null}
+          label={inputText ? `${inputText.length}/100` : null}
           InputProps={{
             endAdornment: (
               <InputAdornment type="button" position="end">
-                <Button className="add-button" variant="contained" type="submit">
+                <Button disabled={error} className="add-button" variant="contained" type="submit">
                   Submit
                   <LibraryAddIcon fontSize="large" />
                 </Button>
